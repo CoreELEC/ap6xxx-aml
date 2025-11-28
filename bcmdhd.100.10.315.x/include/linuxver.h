1dbbdd3f6ad2fdd78adb710820f938ef87320f2a
Portisch <hugo.portisch@yahoo.de>
2025-11-28 10:18:44 +0100

    fix SMP_RD_BARRIER_DEPENDS

--- bcmdhd.100.10.315.x/include/linuxver.h
+++ bcmdhd.100.10.315.x/include/linuxver.h
@@ -635,11 +635,13 @@ static inline bool binary_sema_up(tsk_ctl_t *tsk)
 	return sem_up;
 }
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 0))
+#if  (LINUX_VERSION_CODE > KERNEL_VERSION(5, 6, 0))
+#define SMP_RD_BARRIER_DEPENDS(x)
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 0))
 #define SMP_RD_BARRIER_DEPENDS(x) smp_read_barrier_depends(x)
 #else
 #define SMP_RD_BARRIER_DEPENDS(x) smp_rmb(x)
-#endif // endif
+#endif
 
 #define PROC_START(thread_func, owner, tsk_ctl, flags, name) \
 { \
